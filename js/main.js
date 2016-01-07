9e2503b89aab918011e7be420cf4c483a58d5ea8
seksenov <seksenovk@gmail.com>
2016-01-07 13:44:58 -0800

    Update 2016-01-07T21:44:57.368Z

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("blinkB").addEventListener("click",flipLed);
+	document.getElementById("reload").addEventListener("click",reload);
 })
 
 var gpioController = Windows.Devices.Gpio.GpioController.getDefault();
@@ -10,12 +11,18 @@ pin.write(currentValue);
 pin.setDriveMode(Windows.Devices.Gpio.GpioPinDriveMode.output);
 
 function flipLed(){
-	console.log("Flip Led Invoked");
+	console.log("Flip Led Invoked, Current Value: " + currentValue);
     if (currentValue == Windows.Devices.Gpio.GpioPinValue.high) {
+        console.log("setting pin value low");
         currentValue = Windows.Devices.Gpio.GpioPinValue.low;
     } else {
+    	console.log("Setting value to high");
         currentValue = Windows.Devices.Gpio.GpioPinValue.high;
     }
     pin.write(currentValue);
-    setTimeout(flipLed, 500);
+    //setTimeout(flipLed, 500);
+}
+
+function reload() {
+	document.location.reload(true);
 }
