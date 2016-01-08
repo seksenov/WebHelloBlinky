3ecf77a5b6e246d4f4325a665c04917b5c5074fb
seksenov <seksenovk@gmail.com>
2016-01-08 11:32:42 -0800

    adding slider

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("blinkB").addEventListener("click",flipLed);
 	document.getElementById("bkinkT").addEventListener("click",setBlinkTime);
 	document.getElementById("reload").addEventListener("click",reload);
+	$( "#slider" ).slider();
 })
 
 var gpioController = Windows.Devices.Gpio.GpioController.getDefault();
@@ -12,21 +13,22 @@ pin.write(currentValue);
 pin.setDriveMode(Windows.Devices.Gpio.GpioPinDriveMode.output);
 var timed = false;
 var blinkInterval = 500;
+//var val = $('#slider').slider("option", "value");
 
 function flipLed(){
 	console.log("Flip Led Invoked, Current Value: " + currentValue);
     if (currentValue == Windows.Devices.Gpio.GpioPinValue.high) {
         console.log("setting pin value low");
-        document.getElementById("edgeLogo").className = "";
+        document.getElementById("edgeLogo").className = "myImage";
         currentValue = Windows.Devices.Gpio.GpioPinValue.low;
     } else {
     	console.log("Setting value to high");
-    	document.getElementById("edgeLogo").className = "myImage";
+    	document.getElementById("edgeLogo").className = "";
         currentValue = Windows.Devices.Gpio.GpioPinValue.high;
     }
     pin.write(currentValue);
     if(timed) {
-    	setTimeout(flipLed, blinkInterval);
+    	setTimeout(flipLed, blinkInterval * (100 - $('#slider').slider("option", "value")));
     }
 }
 
